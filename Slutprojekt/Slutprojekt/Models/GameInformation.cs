36c23ffbb48c28244220ed8c237d459daf3812d8
Christofer Ryås <plupp86@gmail.com>
2018-03-27 16:13:11 +0200

    Begin TicTacToe

---
+++ Slutprojekt/Slutprojekt/Models/GameInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slutprojekt.Models
+{
+    public class GameInformation
+    {
+		
+		public string OpponentMame { get; set; }
+		public string Winner { get; set; }
+		public int MarkerPosition { get; set; }
+	}
+}
---
+++ Slutprojekt/Slutprojekt/Models/TicTacToe.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slutprojekt.Models
+{
+	public class TicTacToe
+	{
+		public bool IsGameOver { get; private set; }
+		public bool IsDraw { get; private set; }
+		public Client Player1 { get; set; }
+		public Client Player2 { get; set; }
+
+
+		private readonly int[] field = new int[9];
+		private int movesLeft = 9;
+
+		public TicTacToe()
+		{
+
+			for (int i = 0; i < field.Length; i++)
+			{
+				field[i] = -1;
+			}
+		}
+
+		public bool Play(int player, int position)
+		{
+			if (IsGameOver)
+				return false;
+			
+
+			PlaceMarker(player, position);
+			return CheckWinner();			
+		}
+
+		private bool CheckWinner()
+		{
+			for (int i = 0; i < 3; i++)
+			{
+
+
+
+			}
+		}
+
+
+			
+
+
+
+	}
+}
