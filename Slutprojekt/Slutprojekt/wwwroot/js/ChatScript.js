d2c33024785cdc03dd10e26c9766b681e9fda383
Christofer Ryås <plupp86@gmail.com>
2018-03-28 15:39:52 +0200

    something shaky

---
+++ Slutprojekt/Slutprojekt/wwwroot/js/ChatScript.js
@@ -0,0 +1,31 @@
+﻿$("#waitingForPlayers").hide();
+$("#gameDiv").hide();
+
+$("#startGame").click(function () {
+	$("#startDiv").hide();
+	$("#waitingForPlayers").show();
+});
+
+let connection = new signalR.HubConnection('/chat');
+connection.start();
+
+//var userName = "@Model.UserName.ToString()";
+
+connection.on('onSend', data => {
+	$("#txtChat").append(data + "\n");
+	scrollToBottom();
+});
+
+$("#btnSend").click(() => {
+	let message = userName + ": " + $("#txtMessage").val();
+	connection.invoke('Send', message);
+	$("#txtMessage").val("");
+});
+
+//$("#txtChat").change(function () {
+//	scrollToBottom();
+//});
+
+function scrollToBottom() {
+	$('#txtChat').scrollTop($('#txtChat')[0].scrollHeight);
+}
