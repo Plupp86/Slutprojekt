7aaf35b4d8d161b014405d398a157e1cd50df9df
Christofer Ryås <plupp86@gmail.com>
2018-03-29 09:16:36 +0200

    SQL server now on Azure

--- Slutprojekt/Slutprojekt/Startup.cs
+++ Slutprojekt/Slutprojekt/Startup.cs
@@ -26,6 +26,7 @@ namespace Slutprojekt
 		{
 			this.conf = conf;
 		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -64,16 +65,16 @@ namespace Slutprojekt
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+			//if (env.IsDevelopment())
+			//{
+			//}
+			app.UseDeveloperExceptionPage();
 
 			app.UseStaticFiles();
 
 			app.UseSignalR(routes =>
 			{
-				//routes.MapHub<Chat>("chat");
+				routes.MapHub<Chat>("chat");
 				routes.MapHub<GameHub>("gameHub");
 			});
 
